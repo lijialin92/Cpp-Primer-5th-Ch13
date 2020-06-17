22771f5c1a1cd3fee8ee7ec85c652076f75a6a6f
lijialin <lijialin990@gmail.com>
2020-06-17 19:58:29 +0200

    finished exercise 13.8

--- main.cpp
+++ main.cpp
@@ -25,6 +25,51 @@
  * StrBlobPtr一样的，逐个拷贝自己类型中的成员变量。唯一需要注意的就是shared_ptr拷贝的时候引用计数会+1而StrBlobPtr中的weak_ptr拷
  * 贝的时候，这个weak_ptr依靠的shared_ptr的引用是不+1的。*/
 
+/**exercise 13.4*/
+/*Point global;
+Point foo_bar(Point arg)//当需要传参的时候需要复制构造函数
+{
+    Piont local = arg, *//**这里是复制构造初始化，用到了复制构造函数*//* *heap = new Point(global);
+    *heap = local; //这里应该是普通的复制，而不用到复制构造函数?
+    Point pa[4] = {local, *heap}; //这里用到了复制构造函数，这里用花括号列表初始化数组中的元素
+    return *heap; //这里然会的对象是Point类型的对象，不是引用。所以调用这个函数的那边，还是需要复制构造函数。
+}*/
+
+/**exercise 13.5*/
+class HasPtr{
+public:
+    HasPtr(const std::string& s = std::string()) :
+    ps(new std::string(s)), i(0) {}
+    HasPtr(HasPtr& hasPtr) : ps(new std::string(*(hasPtr.ps))), i(hasPtr.i) {}
+    HasPtr& operator=(const HasPtr(&));
+
+private:
+    std::string* ps;
+    int i;
+};
+
+/**exercise 13.6
+ * 拷贝赋值运算符：
+ * 本身是一个重载的赋值运算符，定义为类的成员函数。左侧运算对象绑定到隐含的this参数，右侧运算对象是与左侧相同类类型的，作为函数的参
+ * 数，函数的返回指向其左侧对象的引用。
+ * 类对象赋值给另外一个类对象的时候，会用到拷贝赋值运算符。例如 a2 = a1, a1, a2 是相同类类型的对象;
+ * 将右侧对象的成员(非static成员)一个一个赋值给左侧对象的对应成员。
+ * 如果一个类没有定义自己的拷贝赋值运算符，那么编译器就为这个类生成一个合成拷贝运算符。对于有些类，还可以定义自己的拷贝赋值运算符来
+ * 起到禁止类对象赋值的效果。*/
+
+/**exercise 13.7
+ * 由于两个类都未定义拷贝赋值运算符，所以编译器自动生成一个合成拷贝赋值运算符给这两个类。
+ * 当StrBlob 对象相互赋值的时候，把data成员赋值。shared_ptr引用次数+1。StrBlobPtr赋值weakptr的时候，用weakptr的类内定义的拷贝赋
+ * 值运算符赋值，curr直接进行内存复制。*/
+
+/**exercise 13.8*/
+HasPtr& HasPtr::operator=(const HasPtr& hasPtr) {
+    if(!ps)
+        delete ps;
+    ps = new std::string(*(hasPtr.ps));
+    i = hasPtr.i;
+    return *this;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
